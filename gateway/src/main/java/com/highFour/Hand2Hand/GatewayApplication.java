ec95dee10fa193157fe624e5ef68aba5c5f414fc
황요한 <hwang-yohan@hwang-yohan-ui-MacBookPro.local>
2025-01-15 17:32:42 +0900

    fix: gateway build.gradle 수정

--- gateway/src/main/java/com/highFour/Hand2Hand/GatewayApplication.java
+++ gateway/src/main/java/com/highFour/Hand2Hand/GatewayApplication.java
@@ -3,8 +3,8 @@ package com.highFour.Hand2Hand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 
+@SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
