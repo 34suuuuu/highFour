e861f88f5b1a90500eec599fa6c13c4491b152c1
황요한 <hwang-yohan@hwang-yohan-ui-MacBookPro.local>
2025-01-15 14:36:39 +0900

    fix: rental 오류 해결

--- rental/src/main/java/com/highFour/Hand2Hand/RentalApplication.java
+++ rental/src/main/java/com/highFour/Hand2Hand/RentalApplication.java
@@ -3,7 +3,6 @@ package com.highFour.Hand2Hand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class RentalApplication {
 
