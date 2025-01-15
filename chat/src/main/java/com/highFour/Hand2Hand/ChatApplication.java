7b60e2e5f872fe3789c899891709296898795b5d
황요한 <hwang-yohan@hwang-yohan-ui-MacBookPro.local>
2025-01-15 10:17:45 +0900

    init: MSA구조 세팅

---
+++ chat/src/main/java/com/highFour/Hand2Hand/ChatApplication.java
@@ -0,0 +1,15 @@
+package com.highFour.Hand2Hand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
